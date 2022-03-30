41a5e98fc7b08ac11a331b8e500c22e2019f700f
Blaine Reid <96265960+Blaine-Reid@users.noreply.github.com>
2022-03-29 17:23:08 -0700

    Update settings.py

--- colorpicker/settings.py
+++ colorpicker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4bh$bhm#-_&a5y9v2$*@cu7fn4y+7^qs#d(_5bbk(m*2t5wy4*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
